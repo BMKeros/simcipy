4cbb14072957cc80bfb85b0ff9887b1c9646842e
Israel Lugo <hostelixisrael@gmail.com>
2019-06-26 13:25:18 -0400

    [DEL] delete folder api

--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
-
-
-urlpatterns = [
-    path('token/create', obtain_jwt_token),
-    path('token/refresh', refresh_jwt_token),
-    path('token/verify', verify_jwt_token),
-]
