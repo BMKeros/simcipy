fd3ac3afcc30ffe945d898002da4b5458345364e
Israel Lugo <hostelixisrael@gmail.com>
2019-06-25 23:35:44 -0400

    [MOD] add config for vue production

--- website/vue.config.js
+++ website/vue.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  outputDir: path.resolve(__dirname, '../static')
+  outputDir: path.resolve(__dirname, './static/website'),
+  indexPath: '../../templates/index.html',
+  publicPath: '/static/website'
 };
