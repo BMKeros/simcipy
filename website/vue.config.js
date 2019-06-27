33c952517479b1403c779a061fbb1a2ecfe1b5c1
Israel Lugo <hostelixisrael@gmail.com>
2019-06-27 10:16:28 -0400

    [REF][ADD][MOD] refactor in module website replace vuetify for quasar

--- website/vue.config.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  outputDir: path.resolve(__dirname, './static/website'),
-  indexPath: '../../templates/index.html',
-  publicPath: '/static/website'
-};
